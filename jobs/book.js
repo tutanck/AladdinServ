5dd3cbf959174b8e36d415350d103517baac3c46
tutanck <tutanck@gmail.com>
2017-10-01 01:29:13 +0200

    user's contacts ids pipeline

--- jobs/book.js
+++ jobs/book.js
@@ -3,8 +3,10 @@ List of jobs
 */
 
 var user_rating = require('./user_rating')
+var user_contacts = require('./user_contacts')
 
 exports.jobs = {
     'getUserRating' : user_rating.getUserRating
+    ,'getUserContacts' : user_contacts.getUserContacts
 }
 
---
+++ jobs/user_contacts.js
@@ -0,0 +1,43 @@
+/*
+'messages' collection's pipeliner
+*/
+
+var coll = 'MESSAGES'
+
+exports.getUserContacts = (regina, bundle) => { 
+    console.log("job::getUserContacts is running with bundle = "+JSON.stringify(bundle))
+    
+    let pipeline =  
+    [
+        {
+            $project: {
+                "senderID": 1, "toID": 1
+            }
+        },
+        { 
+            $match: {
+                "$or" :
+                [
+                    {"senderID": bundle.userID},
+                    {"toID": bundle.userID}
+                ]
+                
+            } 
+        },
+        { 
+            $group :
+            {
+                "_id":null
+                ,"senders":{"$addToSet":"$senderID"}
+                ,"recipients":{"$addToSet":"$toID"}
+            }
+        },
+        { 
+            $project : {
+                "idList":{"$setUnion":["$senders","$recipients"]}
+            }
+        },
+    ];
+    
+    return regina.get(coll).aggregate(pipeline,{});
+}
